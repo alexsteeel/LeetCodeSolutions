e86d3a4e71a4d273e81386697a449a47c2746111
alexsteeel <suchkov.as.1989@gmail.com>
2020-10-22 12:09:17 +0300

    Add unit-tests project.

---
+++ LeetCodeSolutionsUnitTests/Problem1624Tests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace LeetCodeSolutionsUnitTests
+{
+    public class Problem1624Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
